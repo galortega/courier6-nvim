cdb0e3e4359612f7dcef21c177c02b2dfd2fb754
Galo Ortega <galortega@users.noreply.github.com>
2025-02-18 14:11:05 -0500

    add none-ls plugin, plus some plugins modifications

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,6 +9,6 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function () 
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', {})
   end
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,8 +1,8 @@
+vim.cmd("set number")
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.g.maplead = " "
+vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
 
