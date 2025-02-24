5b93d3be1fd4dc5de2619a79df1fb5dd36b32344
Galo Ortega <galortega@users.noreply.github.com>
2025-02-24 15:34:12 -0500

    add mini.vim awesome modules

---
+++ lua/plugins/mini-nvim.lua
@@ -0,0 +1,56 @@
+return {
+	"echasnovski/mini.nvim",
+	version = false, -- Use the latest version (check the repo for specific tags/versions)
+	config = function()
+		-- Surround
+		require("mini.surround").setup({
+			mappings = {
+				add = "sa",
+				delete = "sd",
+				find = "sf",
+				find_left = "sF",
+				highlight = "sh",
+				replace = "sr",
+				update_n_lines = "sn",
+			},
+		})
+
+		-- Cursorword
+		require("mini.cursorword").setup({})
+
+		-- Move
+		require("mini.move").setup({})
+
+		-- Pairs
+		require("mini.pairs").setup({})
+
+		-- Indentscope
+		require("mini.indentscope").setup({
+			options = {
+				try_as_border = true,
+			},
+		})
+
+		-- Animate
+		require("mini.animate").setup({
+			cursor = {
+				enabled = true,
+				priority = 1000,
+				duration = 200,
+				easing = "in_out_sine",
+			},
+			scroll = {
+				enabled = true,
+				priority = 1000,
+				duration = 200,
+				easing = "in_out_sine",
+			},
+		})
+
+		-- Jump2d
+		require("mini.jump2d").setup({})
+
+		-- Bracketed
+		require("mini.bracketed").setup({})
+	end,
+}
