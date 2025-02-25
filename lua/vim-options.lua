f9176a23b8e978c3ae343536d07f83eff03969dc
Galo Ortega <galortega@users.noreply.github.com>
2025-02-25 12:04:40 -0500

    add `.gitignore` and some vim opt configs

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,16 +1,33 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
-vim.opt.swapfile = false
-
 vim.keymap.set('n', '<c-k>', ':wincmd k<cr>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<cr>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<cr>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<cr>')
 
-vim.wo.number = true
-vim.wo.cursorline = true
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv('HOME') .. '/.config/nvim/undodir'
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = 'yes'
+vim.opt.isfname:append("@-@")
+
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+
